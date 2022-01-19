df900b44f54460d829fdd5b6695e4b7aba310b40
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-01-19 21:12:57 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -8,7 +8,7 @@ def detect_object(frame):
     #names_path = os.path.abspath('yolo/obj.names')
 
     # Load Yolo
-    net = cv2.dnn_DetectionModel('yolov4-custom_last.weights','yolov4-custom.cfg')
+    net = cv2.dnn_DetectionModel('yolov4-custom.weights','yolov4-custom.cfg')
     #net = cv2.dnn_DetectionModel(weights_path,cfg_path)
     net.setInputSize(256, 256)
     net.setInputScale(1.0 / 255)
